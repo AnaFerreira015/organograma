c92376a5f742e06d144430b69fdffc37a08d0c45
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2022-09-30 15:20:46 -0300

    Create the team component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Banner from './components/Banner'
 import Form from './components/Form'
+import Team from './components/Team'
 
 function App() {
   const [collaborators, setCollaborators] = useState([])
@@ -14,6 +15,9 @@ function App() {
     <div className="App">
       <Banner />
       <Form registeredContributor={contributor => addedContributor(contributor)} />
+      <Team name="Programação" />
+      <Team name="Front-End" />
+      <Team name="Data Science" />
     </div>
   )
 }
